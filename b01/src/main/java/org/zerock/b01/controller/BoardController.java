5c8a2cab71aaf57218b6ac375bf147744fe766f4
lsy3709 <lsy3709@naver.com>
2024-04-29 15:36:10 +0900

    6교시 댓글 게시글 목록 출력시 해당 댓글의 갯수 표시 BoardController 변경함

--- b01/src/main/java/org/zerock/b01/controller/BoardController.java
+++ b01/src/main/java/org/zerock/b01/controller/BoardController.java
@@ -1,6 +1,5 @@
 package org.zerock.b01.controller;
 
-import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -12,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.b01.dto.BoardDTO;
+import org.zerock.b01.dto.BoardListReplyCountDTO;
 import org.zerock.b01.dto.PageRequestDTO;
 import org.zerock.b01.dto.PageResponseDTO;
 import org.zerock.b01.service.BoardService;
@@ -22,11 +22,14 @@ import org.zerock.b01.service.BoardService;
 @RequiredArgsConstructor
 
 public class BoardController {
+
+  // 게시글에 댓글 갯수 표현하는 목록 출력으로 변경함.
   // 주석으로 내용 변경 확인 테스트
   private final BoardService boardService;
   @GetMapping("/list")
   public void list(PageRequestDTO pageRequestDTO, Model model) {
-    PageResponseDTO<BoardDTO> responseDTO = boardService.list(pageRequestDTO);
+    // 타입 변경 : BoardListReplyCountDTO, 메서드 명 변경.
+    PageResponseDTO<BoardListReplyCountDTO> responseDTO = boardService.listWithReplyCount(pageRequestDTO);
 //    log.info(responseDTO);
     model.addAttribute("responseDTO", responseDTO);
   }
